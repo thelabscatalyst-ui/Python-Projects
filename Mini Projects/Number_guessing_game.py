7940fbfd5b74555bcba8f85a543103865e195d96
Mehersng <thelabscatalyst@gmail.com>
2026-05-14 20:03:44 +0530

    Fix the code

--- Mini Projects/Number_guessing_game.py
+++ Mini Projects/Number_guessing_game.py
@@ -10,7 +10,7 @@ def calculate_score(attempts_used, max_attempts):
     score_negetive_ratio = 100/max_attempts # reduce linearly
 
     if (attempts_used > max_attempts): return 0
-    else: return (100 - (attempts_used - 1) * score_negetive_ratio)
+    else: return (100 - (attempts_used) * score_negetive_ratio)
     pass
 
 def get_hint(guess, secret, max_range):
@@ -32,11 +32,80 @@ def get_hint(guess, secret, max_range):
     pass
 
 def get_difficulty():
-    level = input("Enter 'easy' or 'hard' according to what level do you want to play:- ")
-    
-    if(input == "easy"): return dictionary.fromkeys(easy)
-    
+    bool = False
+    while(bool == False):
+        level = input("Enter 'easy' or 'hard' : ").lower().strip()
+
+        if(level == "easy"): 
+            bool = True
+            return dictionary["easy"]
+        
+        if(level == "hard"):
+            bool = True
+            return dictionary["hard"]
+        
+        else: print("Wrong input, try again:- ")
+        # bool remains false;
     pass
 
+def get_guess(attempt, max_attempts, low, high):
+    bool = False
+    while(bool == False):
+        print("Enter your", attempt, "/", max_attempts, "attempt: ")
+        num = input()
 
+        if(num.isnumeric()):
+            if(int(num) <= high and int(num) >= low):
+                bool = True
+                return int(num)
+            else: 
+                print("Number out of bounds")
+        else: 
+            print("Enter only integer numbers")
+            
+        attempt += 1
+    pass    
 
+def play_round(low, high, max_attempts):
+    number = random.randint(low, high)
+    # using the random module
+
+    attempt = 0
+
+    bool = False
+    while(bool == False and attempt < max_attempts):
+        myguess = get_guess(attempt + 1, max_attempts, low, high)
+        if(myguess == number):
+            print("You won!")
+            print("Your score is: ", calculate_score(attempt, max_attempts))
+            bool = True
+            return calculate_score(attempt, max_attempts)
+        else: 
+            attempt += 1
+            print(get_hint(myguess, number, high))
+
+    if(attempt > max_attempts): 
+        print("You Lost! Answer was: ", number)
+    pass
+
+def main():
+    print("---------- Welcome to the number guessing game ----------")
+    high_score = 0 # currunt high score which is zero, we will update it soon
+
+    bool = False
+    while(bool == False):
+        mode = get_difficulty() # enter the difficulty
+        num = play_round(*mode)
+
+        if(num > high_score): high_score = num
+        print("high score in this sessions: ", high_score)
+
+        play = input("enter y/n or exit whether you want to play the game again? ").lower().strip();
+        if(play == "y"): bool = False
+        elif(play == "n"): bool = True
+        elif(play == "exit"): bool = True
+
+        else: print("Wrong input, try again:- ")
+
+if __name__ == "__main__":
+    main()
